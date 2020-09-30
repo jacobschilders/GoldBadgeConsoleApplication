c6aa986f236ca6a911fcef183e557c359fb81d57
jacobschilders <jschilders@anderson.edu>
2020-09-30 14:19:09 -0400

    9/30 classes

--- Menu.cs
+++ Menu.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace GoldBadgeConsoleApp
 {
-    class Menu
+    //class
+    
+
+    
+    public class MenuItem
+    { //properties
+        public string[] Ingrediants = { "tomato", "onion", "cheese", "bacon", "lettuce", "pickles", "mustard", "ketchup" };
+        public string ItemName { get; set; }
+        public int ItemNumber { get; set; }
+        public string Description { get; set; }
+        
+    //constructors
+    public MenuItem() { }
+    public MenuItem(string[] ingrediants, string itemName, int itemNumber, string description)
     {
+        Ingrediants = ingrediants;
+        ItemName = itemName;
+        ItemNumber = itemNumber;
+        Description = description;
+
     }
+    }
+
+
+
 }
